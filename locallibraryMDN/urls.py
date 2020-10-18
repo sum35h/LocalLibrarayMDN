50cd71155779bc08e09917b980008ec99a9973e7
Sumesh Pradhan <sumesh.1rn14cs107@gmail.com>
2020-10-18 15:52:03 +0530

    Created models for Book, Author,BookInstance and Genre

--- locallibraryMDN/urls.py
+++ locallibraryMDN/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/',include('catalog.urls')),
+    #path('', RedirectView.as_view(url='catalog/', permanent=True)),
+   # path('catalog/',include('catalog.urls')),
+    #static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
