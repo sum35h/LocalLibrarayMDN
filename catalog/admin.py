68e45059873e211e2f21c2e722537c9d3328e89e
Sumesh Pradhan <sumesh.1rn14cs107@gmail.com>
2020-10-18 17:27:39 +0530

    Registered ModelAdmin class for the Models with custom list_view and filters

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
 from .models import Author,Genre,Book,BookInstance
 
+class BooksInstanceInline(admin.TabularInline):
+    extra=0
+    model = BookInstance
 
-admin.site.register(Book)
+class BookInline(admin.TabularInline):
+    extra=0
+    model = Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name','first_name','date_of_birth','date_of_death')
+    fields = ['first_name','last_name',('date_of_birth','date_of_death')]
+    inlines = [BookInline]
+
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    
+    list_display = ('title','author','display_genre')
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status','due_back')
+
+    fieldsets = (
+        (None, {'fields':('book','imprint','id')}),
+        ('Availability',{'fields':('status','due_back')})
+    )
+
+    list_display=('book','status','due_back','id')
+
+# admin.site.register(Book)
 admin.site.register(Genre)
-admin.site.register(Author)
-admin.site.register(BookInstance)
+admin.site.register(Author,AuthorAdmin)
+# admin.site.register(BookInstance)
