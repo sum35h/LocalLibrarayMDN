c8f813e78aedd276723898a2acbe275d90edd2e7
Sumesh Pradhan <sumesh.1rn14cs107@gmail.com>
2020-10-18 18:36:07 +0530

    updated url mappings

--- locallibraryMDN/urls.py
+++ locallibraryMDN/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import RedirectView
-# from django.conf.urls.static import static
-# from django.conf import settings
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -10,5 +10,5 @@ urlpatterns = [
 
     path('catalog/',include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
-    #static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
