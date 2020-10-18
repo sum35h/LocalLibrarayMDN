02b0ba25dc8812d3b0ebf4516bb35dd3d4efb82c
Sumesh Pradhan <sumesh.1rn14cs107@gmail.com>
2020-10-18 21:42:11 +0530

    updated urls for catalog detailed books view

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from catalog.models import Book,Author,BookInstance,Genre
+from django.views import generic
+from django.shortcuts import get_object_or_404
 
 def index(request):
     """View function for the home page of site"""
@@ -19,3 +21,27 @@ def index(request):
     }
 
     return render(request,'index.html',context=context)
+
+
+class BookListView(generic.ListView):
+    model=Book
+    context_object_name = 'book_list'
+    #queryset = Book.objects.filter(title__icontains='the')
+    template_name = 'catalog/book_list.html'
+
+    def get_queryset(self):
+        return Book.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+
+        context['some_data']='This is just some data'
+        return context
+
+class BookDetailView(generic.DetailView):
+    model=Book
+    # template_name = 'catalog/book_detail.html'
+    # context_object_name = 'book'
+    # def get_queryset(self):
+    #     book = get_object_or_404(Book,pk=primary_key)
+    #     return book
