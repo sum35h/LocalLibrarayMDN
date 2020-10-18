141c380a70860d441776b971acf640c5a337b819
Sumesh Pradhan <sumesh.1rn14cs107@gmail.com>
2020-10-18 18:05:20 +0530

    updated the url for app and catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpattens=[
+urlpatterns=[
         path('',views.index, name='index'),
 ]
--- locallibraryMDN/urls.py
+++ locallibraryMDN/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import RedirectView
-from django.conf.urls.static import static
-from django.conf import settings
+# from django.conf.urls.static import static
+# from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', RedirectView.as_view(url='catalog/', permanent=True)),
-   # path('catalog/',include('catalog.urls')),
+
+    path('catalog/',include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
     #static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
