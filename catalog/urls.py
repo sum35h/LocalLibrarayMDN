d64af0ddc304cda13902e16ed475dbe7d7d80a3a
Sumesh Pradhan <sumesh.1rn14cs107@gmail.com>
2020-11-01 21:02:38 +0530

    Added new urls for mybooks, allbooks, book renew and author create,edit and delete

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,11 @@ urlpatterns=[
        path('books/<int:pk>',views.BookDetailView.as_view(), name='book-detail'),
         path('authors/',views.AuthorListView.as_view(),name='authors'),
         path('authors/<int:pk>',views.AuthorDetailView.as_view(),name='author-detail'),
+         path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+         path('allborrowed/',views.AllBorrowedBooks.as_view(),name='all-borrowed'),
+             path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+                path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+
 ]
