3289334cd42b2a69f1e1a10691e04ef6bd8c89fa
Sumesh Pradhan <sumesh.1rn14cs107@gmail.com>
2020-11-01 21:06:00 +0530

    Added permissions for bookinstance and author models

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 import uuid
+from django.contrib.auth.models import User
+from datetime import date
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -43,6 +45,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book',on_delete=models.SET_NULL,null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True,blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m','Maintenance'),
@@ -55,10 +58,17 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)   
 
     def __str__(self):
         """String for representing the Model Object"""
         return f"{self.id} ({self.book.title})"
+    
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
 
 class Author(models.Model):
@@ -70,6 +80,8 @@ class Author(models.Model):
 
     class Meta:
         ordering = ['last_name','first_name']
+        permissions = (("can_create_author", "create author"),("can_edit_author", "edit author"),("can_delete_author", "delete author"))   
+
 
     def get_absolute_url(self):
         """Returns the url to access the particular author instance"""
