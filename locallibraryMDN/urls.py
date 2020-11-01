282885d87177abfdbc35e27674a2813baf3d8045
Sumesh Pradhan <sumesh.1rn14cs107@gmail.com>
2020-11-01 21:07:22 +0530

    added path for accounts

--- locallibraryMDN/urls.py
+++ locallibraryMDN/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('accounts/', include('django.contrib.auth.urls')),
     path('catalog/',include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
